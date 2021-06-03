aa6482462a5acdee3207d9a0be4c983163ffb514
Andrej Stevkovski <stevandrej@yahoo.com>
2021-06-03 22:56:02 +0200

    Revert "Basic logic and some css"
    This reverts commit 8faa3ca3efb2125304e77293bf4771e255312af0.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React, { useContext } from 'react';
-import AddTodo from './components/AddTodo/AddTodo';
-import TodoList from './components/TodoList/TodoList';
-import { TodoContext } from './store/todo-context';
-import './App.scss';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-	const todosCtx = useContext(TodoContext);
-
-	return (
-		<div className="App">
-			<div className='bg-image'></div>
-			<div className="container">
-				<div className='input-form'>
-					<AddTodo />
-				</div>
-				<TodoList />
-				<button onClick={todosCtx.clearAllCompleted}>Clear All Completed</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
